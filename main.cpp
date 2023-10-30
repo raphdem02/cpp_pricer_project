db505b0806b7935c9c00eb0b997034d744833941
raphdem02 <raph.Demare@gmail.com>
2023-10-30 16:09:07 +0100

    test put

--- main.cpp
+++ main.cpp
@@ -9,15 +9,18 @@
 int main()
 {
     double S0(95.), K(100.), T(0.5), r(0.02), sigma(0.2);
+    #pragma region Part1    
     {
+        
         CallOption opt1(T, K);
         PutOption opt2(T, K);
-
-        BlackScholesPricer BSpricer(&opt1, S0, r, sigma);
-        std::cout << "call price = " << BSpricer() << ", delta = " << BSpricer.delta() << std::endl;
+        BlackScholesPricer BSpricerCall(&opt1, S0, r, sigma);
+        BlackScholesPricer BSpricerPut(&opt2, S0, r, sigma);
+        std::cout << "######################### Black Scholes Model ################################" << std::endl;
+        std::cout << "call price = " << BSpricerCall() << ", delta = " << BSpricerCall.delta() << std::endl;
+        std::cout << "put price = " << BSpricerPut() << ", delta = " << BSpricerPut.delta() << std::endl;
         
 
     }
-
-    return 0;
+    #pragma endregion
 }
